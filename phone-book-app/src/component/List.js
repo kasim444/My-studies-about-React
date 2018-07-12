1f528ad112f08b5457df7eef8a141cc1efa86e2a
Kasim <kasim1411@gmail.com>
2018-07-12 13:51:41 +0300

    Phone Book App
    Last Version

---
+++ phone-book-app/src/component/List.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class List extends Component {
+    state = {};
+    static defaultProps = {
+        
+    };
+
+    static propTypes = {};
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default List;
